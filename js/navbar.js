57c0e8a67f8d018658cc112e034882ac0fc3a496
danny-elvis <65183303+danny-elvis@users.noreply.github.com>
2020-06-18 13:58:29 -0300

    Add files via upload

--- js/navbar.js
+++ js/navbar.js
@@ -1,23 +1,25 @@
 
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos < currentScrollPos) {
-    document.getElementById("navbar").style.bottom = "0";
-  } else {
-    document.getElementById("navbar").style.bottom = "-4em";
-  }
-  prevScrollpos = currentScrollPos;
-} 
+var prevScrollPos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    console.log("atual:", currentScrollPos)
+    console.log("anterior:", prevScrollPos)
+    if (prevScrollPos < currentScrollPos || currentScrollPos < '5' || (prevScrollPos/3) > (currentScrollPos/2)) {
+        document.getElementById("navbar").style.bottom = "0";
+    } else {
+        document.getElementById("navbar").style.bottom = "-4em";
+    }
+    prevScrollPos = currentScrollPos;
+}
 
 
 var header = document.getElementById("navbar");
 var btns = header.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
-  });
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
 }
