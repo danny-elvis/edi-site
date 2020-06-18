57d322bb4983ab584d192ed9249a77ca847de80c
danny-elvis <65183303+danny-elvis@users.noreply.github.com>
2020-06-18 18:28:35 -0300

    Add files via upload

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,17 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollPos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollPos < currentScrollPos || currentScrollPos < '5' || (prevScrollPos / 3) > (currentScrollPos / 2)) {
+        document.getElementById("navbar").style.bottom = "0";
+    } else {
+        document.getElementById("navbar").style.bottom = "-4em";
+    }
+    prevScrollPos = currentScrollPos;
+}
+
+
+
 // Cache selectors
 var lastId,
     topMenu = $("#navbar"),
@@ -14,10 +28,10 @@ var lastId,
 // so we can get a fancy scroll animation
 menuItems.click(function(e){
   var href = $(this).attr("href"),
-      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+      offsetTop = href === "#" ? 0 : $(href).position().top;//-topMenuHeight+1; offset->position
   $('html, body').stop().animate({ 
       scrollTop: offsetTop
-  }, 300);
+  }, 500);
   e.preventDefault();
 });
 
@@ -28,7 +42,7 @@ $(window).scroll(function(){
    
    // Get id of current scroll item
    var cur = scrollItems.map(function(){
-     if ($(this).offset().top < fromTop)
+     if ($(this).position().top < fromTop) //offset
        return this;
    });
    // Get the id of the current element
@@ -40,28 +54,18 @@ $(window).scroll(function(){
        // Set/remove active class
         $('a').removeClass("active")
         $("[href='#"+id+"']").addClass("active");
+        document.getElementById("navbar").style.bottom = "0";
        /* menuItems
          .parent().removeClass("active")
          .end().filter("[href='#"+id+"']").parent().addClass("active"); */
    }                   
 });
 
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollPos = window.pageYOffset;
-window.onscroll = function () {
-    var currentScrollPos = window.pageYOffset;
-    console.log("atual:", currentScrollPos)
-    console.log("anterior:", prevScrollPos)
-    if (prevScrollPos < currentScrollPos || currentScrollPos < '5' || (prevScrollPos/3) > (currentScrollPos/2)) {
-        document.getElementById("navbar").style.bottom = "0";
-    } else {
-        document.getElementById("navbar").style.bottom = "-4em";
-    }
-    prevScrollPos = currentScrollPos;
-}
 
 
-var header = document.getElementById("navbar");
+
+
+/* var header = document.getElementById("navbar");
 var btns = header.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function () {
@@ -69,4 +73,4 @@ for (var i = 0; i < btns.length; i++) {
         current[0].className = current[0].className.replace(" active", "");
         this.className += " active";
     });
-}
+} */
