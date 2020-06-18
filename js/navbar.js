95cf184065b51522b6d6c69840c8da4878b0d4ca
danny-elvis <65183303+danny-elvis@users.noreply.github.com>
2020-06-18 13:13:18 -0300

    Add files via upload

---
+++ js/navbar.js
@@ -0,0 +1,23 @@
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos < currentScrollPos) {
+    document.getElementById("navbar").style.bottom = "0";
+  } else {
+    document.getElementById("navbar").style.bottom = "-4em";
+  }
+  prevScrollpos = currentScrollPos;
+} 
+
+
+var header = document.getElementById("navbar");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
